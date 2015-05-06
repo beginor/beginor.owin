a44af8b3c262bd8afcc8d6f2318261bdb98d98d0
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2015-05-07 07:42:52 +0800

    init check in.

---
+++ src/Beginor.Owin.Loging/CastleLoggerFactory.cs
@@ -0,0 +1,18 @@
+﻿using Castle.Core.Logging;
+
+namespace Beginor.Owin.Logging {
+
+    public class CastleLoggerFactory : Microsoft.Owin.Logging.ILoggerFactory {
+
+        private ILoggerFactory loggerFactory;
+
+        public CastleLoggerFactory(ILoggerFactory loggerFactory) {
+            this.loggerFactory = loggerFactory;
+        }
+
+        public Microsoft.Owin.Logging.ILogger Create(string name) {
+            var logger = loggerFactory.Create(name);
+            return new CastleLogger(logger);
+        }
+    }
+}
---
+++ src/Beginor.Owin.Loging/LoggingExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin.Logging;
+using Owin;
+
+namespace Beginor.Owin.Logging {
+
+    public static class LoggingExtensions {
+
+        public static void UseCastleLogging(this IAppBuilder app) {
+            
+        }
+    }
+}
