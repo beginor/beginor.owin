438bd5b27db3d5d9b49a471f0bf572a95db863e0
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2015-05-13 07:01:07 +0800

    add beginor.owin.windsor .

---
+++ src/Beginor.Owin.Windsor/Extensions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Owin;
+using Castle.Windsor;
+using Castle.Windsor.Installer;
+
+namespace Beginor.Owin.Windsor {
+
+    public static class Extensions {
+
+        private static readonly string AppContainerName = "Beginor.Owin.Windsor"
+
+        public static void UseWindsorContainer(this IAppBuilder app, IWindsorContainer container) {
+            if (app == null) {
+                throw new ArgumentNullException("app");
+            }
+            if (container == null) {
+                throw new ArgumentNullException("container");
+            }
+            if (app.Properties.ContainsKey(AppContainerName)) {
+                throw new InvalidOperationException("Container already exists.");
+            }
+            app.Properties.Add(AppContainerName, container);
+        }
+
+        public static void UseWindsorContainer(this IAppBuilder app, string path) {
+            if (app == null) {
+                throw new ArgumentNullException("app");
+            }
+            if (string.IsNullOrEmpty(path)) {
+                throw new ArgumentNullException("path");
+            }
+            IWindsorContainer container = new WindsorContainer();
+            container.Install(
+                Configuration.FromXmlFile("windsor.config")
+            );
+            app.UseWindsorContainer(app, container);
+        }
+
+        public static IWindsorContainer GetWindsorContainer(this IAppBuilder app) {
+            if (app == null) {
+                throw new ArgumentNullException("app");
+            }
+            if (!app.Properties.ContainsKey(AppContainerName)) {
+                throw new InvalidOperationException("Windsor container is not configured.");
+            }
+            return (IWindsorContainer)app.Properties[AppContainerName];
+        }
+
+    }
+}
