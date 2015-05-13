d9fe3df5eab3cf05c7684e6cc4154fcaec11ccb0
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2015-05-14 07:54:15 +0800

    castle exception logger.

---
+++ src/Beginor.Owin.WebApi.Windsor/CastleExceptionLogger.cs
@@ -0,0 +1,21 @@
+﻿using Castle.Core.Logging;
+using System.Web.Http.ExceptionHandling;
+
+namespace Beginor.Owin.WebApi.Windsor {
+
+    public class CastleExceptionLogger : ExceptionLogger {
+
+        private ILogger logger = NullLogger.Instance;
+
+        public ILogger Logger {
+            get { return logger; }
+            set { logger = value; }
+        }
+
+        public override void Log(ExceptionLoggerContext context) {
+            Logger.ErrorFormat(context.Exception, "Exception caught processing request {0}", context.Request.RequestUri);
+        }
+
+    }
+
+}
