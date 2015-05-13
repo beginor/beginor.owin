7e9fe79e101fdd8ff94680d2f1d2b036b04684ee
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2015-05-13 21:49:08 +0800

    register container itself.

--- src/Beginor.Owin.Windsor/Extensions.cs
+++ src/Beginor.Owin.Windsor/Extensions.cs
@@ -2,6 +2,7 @@
 using Owin;
 using Castle.Windsor;
 using Castle.Windsor.Installer;
+using Castle.MicroKernel.Registration;
 
 namespace Beginor.Owin.Windsor {
 
@@ -9,7 +10,8 @@ namespace Beginor.Owin.Windsor {
 
         private static readonly string AppContainerName = "Beginor.Owin.Windsor";
 
-        public static void UseWindsorContainer(this IAppBuilder app, IWindsorContainer container) {
+        public static void UseWindsorContainer(this IAppBuilder app,
+                                               IWindsorContainer container) {
             if (app == null) {
                 throw new ArgumentNullException("app");
             }
@@ -22,7 +24,8 @@ namespace Beginor.Owin.Windsor {
             app.Properties.Add(AppContainerName, container);
         }
 
-        public static void UseWindsorContainer(this IAppBuilder app, string path) {
+        public static void UseWindsorContainer(this IAppBuilder app,
+                                               string path) {
             if (app == null) {
                 throw new ArgumentNullException("app");
             }
@@ -32,6 +35,9 @@ namespace Beginor.Owin.Windsor {
             IWindsorContainer container = new WindsorContainer();
             container.Install(
                 Configuration.FromXmlFile("windsor.config")
+            );
+            container.Register(
+                Component.For<IWindsorContainer>().Instance(container)
             );
             app.UseWindsorContainer(container);
         }
