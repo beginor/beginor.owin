3b5d2f3bb2ab43dc2cc4c3f402ca9d9ad6e8ee39
beginor <beginor@qq.com>
2016-01-12 07:25:28 +0800

    use new c#6 syntax

--- src/Beginor.Owin.StaticFile/StaticFileMiddlewareOptions.cs
+++ src/Beginor.Owin.StaticFile/StaticFileMiddlewareOptions.cs
@@ -1,49 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
 namespace Beginor.Owin.StaticFile {
-
-    using AppFunc = Func<IDictionary<string, object>, Task>;
 
     public class StaticFileMiddlewareOptions {
 
-        string defaultFile = "index.html";
-        string rootDirectory = "wwwroot";
-        bool enableETag = true;
-        bool enableHtml5LocationMode = false;
+        public string RootDirectory { get; set; } = "wwwroot";
 
-        public string RootDirectory {
-            get {
-                return rootDirectory;
-            }
-            set {
-                rootDirectory = value;
-            }
-        }
+        public string DefaultFile { get; set; } = "index.html";
 
-        public string DefaultFile {
-            get {
-                return defaultFile;
-            }
-            set {
-                defaultFile = value;
-            }
-        }
+        public bool EnableETag { get; set; } = true;
 
-        public bool EnableETag {
-            get {
-                return enableETag;
-            }
-            set {
-                enableETag = value;
-            }
-        }
-
-        public bool EnableHtml5LocationMode {
-            get { return enableHtml5LocationMode; }
-            set { enableHtml5LocationMode = value; }
-        }
+        public bool EnableHtml5LocationMode { get; set; } = false;
 
         public IMimeTypeProvider MimeTypeProvider { get; set; }
 
