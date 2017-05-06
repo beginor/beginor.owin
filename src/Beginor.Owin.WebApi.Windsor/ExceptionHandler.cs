c918ca0769769524c4546d524f35c488fd90124f
beginor <beginor@qq.com>
2017-05-06 19:11:26 +0800

    add exception handler

---
+++ src/Beginor.Owin.WebApi.Windsor/ExceptionHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+
+namespace Beginor.Owin.WebApi.Windsor {
+
+    public class ExceptionHandler : System.Web.Http.ExceptionHandling.ExceptionHandler {
+
+        public override void Handle(ExceptionHandlerContext context) {
+            var request = context.Request;
+            var ex = context.Exception;
+            var response = request.CreateErrorResponse(
+                HttpStatusCode.InternalServerError,
+                ex.Message
+            );
+            var result = new ResponseMessageResult(response);
+            context.Result = result;
+        }
+
+    }
+
+}
